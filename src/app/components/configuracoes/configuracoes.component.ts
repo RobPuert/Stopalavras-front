32ff642eb5c64ad0f595b56b85010c4891820d15
ROBSON F PUERT <rob_puert@hotmail.com>
2025-07-09 04:33:25 -0300

    configuracao de rotas dos botoes, ajustes no css da home e configuracoes

--- src/app/components/configuracoes/configuracoes.component.ts
+++ src/app/components/configuracoes/configuracoes.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-configuracoes',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './configuracoes.component.html',
   styleUrl: './configuracoes.component.css'
 })
